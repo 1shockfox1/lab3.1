622d225525263605045f82c2efa66cecc6c81fc2
user <user@DESKTOP-DV6KRKO>
2024-10-29 22:17:37 +0200

    Добавьте файлы проекта.

---
+++ lab3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    double x = 24;
+    Console.WriteLine("так изначально у них было " + x + "$");
+    Console.WriteLine("Введите сегодняшний год");
+    double god = double.Parse(Console.ReadLine());
+    double itog = god - 1826;
+    for (int i = 0; i < itog; i++)
+    {
+        x *= 1.06;
+        
+    }
+    Console.WriteLine($"{x:F2} $");
+}
+catch
+{
+    Console.WriteLine("ошибочка 404.....");
+}
+
